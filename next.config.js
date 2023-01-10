74cf0a13d44217feed7dc3603a1d5d7e05293cce
Matias Bacelar <matiasbacelar@gmail.com>
2023-01-10 17:06:46 -0300

    Eslint, prettier and aliases setup

--- next.config.js
+++ next.config.js
@@ -2,4 +2,5 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+// eslint-disable-next-line
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
+import Test from '@/components/Test';
+
 const Home = () => {
-  const version: number = 34;
+  const version = '12.2.0';
 
   return (
     <div>
       <h1>Custom Next JS & Typescript setup</h1>
       <p>Next Version: {version}</p>
+
+      <Test />
     </div>
   );
 };
