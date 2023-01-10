723b67127d2967b2cf9a306e4809424d1f2fc507
Matias Bacelar <matiasbacelar@gmail.com>
2023-01-10 19:31:16 -0300

    Styled components setup

--- components/Test/Test.tsx
+++ components/Test/Test.tsx
@@ -1,3 +1,13 @@
-const Test = () => <div>Test</div>;
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  color: red;
+`;
+
+const Test = () => (
+  <div>
+    <Heading>Test</Heading>
+  </div>
+);
 
 export default Test;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 // eslint-disable-next-line
