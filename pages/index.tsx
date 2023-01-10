aac379678226aeccf02295af5869ef97c6ded644
Matias Bacelar <matiasbacelar@gmail.com>
2023-01-10 16:41:02 -0300

    Basic setup with ts

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  const version: number = 34;
+
+  return (
+    <div>
+      <h1>Custom Next JS & Typescript setup</h1>
+      <p>Next Version: {version}</p>
+    </div>
+  );
+};
+
+export default Home;
