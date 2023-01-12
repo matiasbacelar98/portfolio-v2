00c800708424e43942cb0322a06aa26e66d7e750
Matias Bacelar <matiasbacelar@gmail.com>
2023-01-11 23:31:25 -0300

    spacing values change to rems

--- styles/variables.ts
+++ styles/variables.ts
@@ -1,14 +1,14 @@
 import { css } from 'styled-components';
 
 type ValuesType = {
-  [key: string]: number;
+  [key: string]: string;
 };
 
 const generateSpacingValues = (levels: number, base = 8) => {
   const values: ValuesType = {};
 
   for (let i = 0; i <= levels; i++) {
-    values[`--sp-${i + 1}`] = base * (i + 1);
+    values[`--sp-${i + 1}`] = `${(base * (i + 1)) / 16}rem`;
   }
 
   return values;
@@ -20,7 +20,7 @@ const variables = css`
     // Fonts
     --font: 'HK Grotesk', sans-serif;
     // Spacing
-    ${generateSpacingValues(20)}
+    ${generateSpacingValues(40)}
     // Others
     --border-radius: 0.5rem;
     --box-shadow: -2px 2px 15px -2px rgba(0, 0, 0, 0.3);
