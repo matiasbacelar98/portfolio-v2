f5995bca21d3af9147d5d35b491073ee202d5633
Matias Bacelar <matiasbacelar@gmail.com>
2023-01-30 17:02:23 -0300

    favicon added

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -10,6 +10,7 @@ const Head = ({ title, description }: HeadProps) => {
     <NextHead>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
 };
